ab08882077901fc4583b4bd21a76e5ef09b81581
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-02-01 17:44:47 -0300

    feat: adds UUID and request/response typing

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,21 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { randomUUID } from "node:crypto";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  categories.push({ name, description });
+  const category = {
+    id: randomUUID(),
+    name,
+    description,
+    created_at: new Date(),
+  };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
