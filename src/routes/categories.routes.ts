b7e90c4a518d6c9f29acfb251f83ab1fed268546
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-02-01 18:33:30 -0300

    refactor: ID created in Category class

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,25 @@
 import { Request, Response, Router } from "express";
-import { randomUUID } from "node:crypto";
+
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: randomUUID(),
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
     created_at: new Date(),
-  };
+  });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
