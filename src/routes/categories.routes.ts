5b5f9496109eef0ce2fc218838ec47fc9d68873a
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-02-26 16:20:24 -0300

    feat: adds dependency injection in import category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from "express";
 import multer from "multer";
 
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
-import importCategoryController from "../modules/cars/useCases/importCategory";
+import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
 import listCategoriesController from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
@@ -12,6 +12,7 @@ const upload = multer({
 });
 
 const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
 
 categoriesRoutes.post("/", createCategoryController.handle);
 
@@ -22,9 +23,7 @@ categoriesRoutes.get("/", (request: Request, response: Response) => {
 categoriesRoutes.post(
   "/import",
   upload.single("file"),
-  (request: Request, response: Response) => {
-    return importCategoryController().handle(request, response);
-  }
+  importCategoryController.handle
 );
 
 export { categoriesRoutes };
