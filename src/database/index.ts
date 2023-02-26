6c85d6a115cdc51ac8239f6d4d5f67e6256b1fb3
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-02-26 17:35:07 -0300

    feat: create specification migration

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,15 +1,16 @@
 import { DataSource } from "typeorm";
 
 import { Category } from "../modules/cars/entities/Category";
+import { Specification } from "../modules/cars/entities/Specification";
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: "database",
+  host: "database", // process.env.POSTGRES_HOST
   port: 5432,
   username: "docker",
   password: "1234",
   database: "rentx",
-  entities: [Category],
+  entities: [Category, Specification],
   migrations: ["src/database/migrations/*.ts"],
 });
 
