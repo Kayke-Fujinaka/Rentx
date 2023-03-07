110eeb71a753314586e82ca3652f818e8ec0cf90
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-07 11:28:56 -0300

    feat: adds entity User

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { DataSource } from "typeorm";
 
+import { User } from "../modules/accounts/entities/User";
 import { Category } from "../modules/cars/entities/Category";
 import { Specification } from "../modules/cars/entities/Specification";
 
@@ -13,7 +14,7 @@ const AppDataSource = new DataSource({
   username: "docker",
   password: "1234",
   database: "rentx",
-  entities: [Category, Specification],
+  entities: [Category, Specification, User],
   migrations: ["src/database/migrations/*.ts"],
 });
 
