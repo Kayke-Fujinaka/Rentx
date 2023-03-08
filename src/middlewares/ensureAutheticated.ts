2d4531d23494065a3cccd8d1b7179dffeba14d88
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-08 12:21:58 -0300

    feat: authenticate route

---
+++ src/middlewares/ensureAutheticated.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+import { HttpError } from "../shared/utils/HttpError";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) throw new HttpError("Token missing!", 401);
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "054b9ba25573c70ceeb2411294ca9b42"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = usersRepository.findById(user_id);
+
+    if (!user) throw new HttpError("User does not exists!", 400);
+
+    next();
+  } catch (error) {
+    throw new HttpError("Invalid token!", 401);
+  }
+}
