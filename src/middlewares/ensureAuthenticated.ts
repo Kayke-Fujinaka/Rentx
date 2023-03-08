cac4cda2e55af2b8ae716ecd15e24bf354078947
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-08 13:55:20 -0300

    wip: ensure authenticate middleware

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -15,7 +15,11 @@ export async function ensureAuthenticated(
 ) {
   const authHeader = request.headers.authorization;
 
+  console.log("1");
+
   if (!authHeader) throw new HttpError("Token missing!", 401);
+
+  console.log("1.5");
 
   const [, token] = authHeader.split(" ");
 
@@ -29,10 +33,16 @@ export async function ensureAuthenticated(
 
     const user = await usersRepository.findById(user_id);
 
+    console.log("2");
+
     if (!user) throw new HttpError("User does not exists!", 400);
+
+    console.log("2.5");
 
     next();
   } catch (error) {
+    console.log("3");
+
     throw new HttpError("Invalid token!", 401);
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import "reflect-metadata";
 import "./database";
 import "./shared/container";
 
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import { HttpError } from "./shared/utils/HttpError";
 import swaggerFile from "./swagger.json";
 
 const app = express();
@@ -13,6 +14,15 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof HttpError)
+      return response.status(error.statusCode).json({ message: error.message });
+
+    return response.status(500).json({ message: "Internal server error" });
+  }
+);
 
 app.use(router);
 
