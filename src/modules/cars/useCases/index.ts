8169db0f9060ee778b7e8ebeefb4c1fb92f04c7a
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-11 17:18:07 -0300

    chore: creates barrel exports

---
+++ src/modules/cars/useCases/index.ts
@@ -0,0 +1,4 @@
+export * from "./createCategory/CreateCategoryUseCase";
+export * from "./createSpecification/CreateSpecificationUseCase";
+export * from "./importCategory/ImportCategoryUseCase";
+export * from "./listCategories/listCategoriesUseCase";
