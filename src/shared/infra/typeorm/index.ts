a8c492c8d324b8b7ec87cb6f174f23b816edf29e
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-12 16:46:47 -0300

    feat: adds Car entity

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import { DataSource } from "typeorm";
 
 import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { Category } from "@modules/cars/infra/typeorm/entities/Category";
 import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
 
@@ -14,7 +15,7 @@ const AppDataSource = new DataSource({
   username: "docker",
   password: "1234",
   database: "rentx",
-  entities: [Category, Specification, User],
+  entities: [Category, Specification, User, Car],
   migrations: ["src/shared/infra/typeorm/migrations/*.ts"],
 });
 
