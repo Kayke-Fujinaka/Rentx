f091180c9da4acd382f44335c2066ea265ac19fc
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-14 12:29:37 -0300

    feat: adds injectable and inject

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from "tsyringe";
+
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
@@ -7,11 +9,14 @@ interface IRequest {
   brand?: string;
 }
 
+@injectable()
 class ListAvailableCarsUseCase {
-  constructor(private carsRepository: ICarsRepository) {}
+  constructor(
+    @inject("CarsRepository") private carsRepository: ICarsRepository
+  ) {}
 
   async execute({ category_id, name, brand }: IRequest): Promise<Car[]> {
-    const cars = await this.carsRepository.findAvailable(
+    const cars = await this.carsRepository.findAvailableCars(
       category_id,
       name,
       brand
