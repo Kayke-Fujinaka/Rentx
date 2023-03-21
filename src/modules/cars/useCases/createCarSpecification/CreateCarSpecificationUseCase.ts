208d8b5f7d1e3bb75fcffa483451d9cada45aa97
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-03-21 16:56:20 -0300

    feat: creates create car specification use case

---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -0,0 +1,38 @@
+import { inject } from "tsyringe";
+
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
+import { HttpError } from "@shared/errors/HttpError";
+
+interface IRequest {
+  car_id: string;
+  specifications_id: string[];
+}
+
+class CreateCarSpecificationUseCase {
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository,
+    @inject("SpecificationsRepository")
+    private specificationsRepository: ISpecificationsRepository
+  ) {}
+
+  async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
+    const carExists = await this.carsRepository.findById(car_id);
+
+    if (!carExists) throw new HttpError("Cars does not exists!");
+
+    const specifications = await this.specificationsRepository.findByIds(
+      specifications_id
+    );
+
+    carExists.specifications = specifications;
+
+    await this.carsRepository.create(carExists);
+
+    return carExists;
+  }
+}
+
+export { CreateCarSpecificationUseCase };
