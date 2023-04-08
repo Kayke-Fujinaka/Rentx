68327b4ac1997d49be0ea00deab9680a4ee087d6
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-04-08 16:17:25 -0300

    test: should be able to create a new rental

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -12,10 +12,13 @@ describe("Create Rental", () => {
   });
 
   it("should be able to create a new rental", async () => {
-    await createRentalUseCase.execute({
+    const rental = await createRentalUseCase.execute({
       user_id: "12345",
       car_id: "12345",
       expected_return_date: new Date(),
     });
+
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
   });
 });
