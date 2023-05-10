7ad55b1b7e6badb68e3e3fa1de38fd5745f6cae0
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-10 11:07:28 -0300

    feat: creates auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  secret_token: "054b9ba25573c70ceeb2411294ca9b42",
+  secret_refresh_token: "63cbb17ddf0c6befdd522fda62b48e4e",
+  expires_in_token: "15m",
+  expires_in_refresh_token: "30d",
+};
