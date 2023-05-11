281100bd1bba9057f0c5456aef802cc960c92c7d
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-11 11:30:32 -0300

    refactor: order methods

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -1,9 +1,9 @@
 interface IDateProvider {
+  dateNow(): Date;
+  addDays(days: number): Date;
   compareInHours(start_date: Date, end_date: Date): number;
   compareInDays(start_date: Date, end_date: Date): number;
   convertToUTC(date: Date): string;
-  dateNow(): Date;
-  addDays(days: number): Date;
 }
 
 export { IDateProvider };
