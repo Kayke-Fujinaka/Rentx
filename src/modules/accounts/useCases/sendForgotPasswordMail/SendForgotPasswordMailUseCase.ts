6eb8fb28f836e295e4e5a8108d2b5398b67d5b28
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-22 10:40:40 -0300

    feat: send email in send forgot password

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -4,18 +4,23 @@ import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 import { HttpError } from "@shared/errors/HttpError";
 
 @injectable()
 class SendForgotPasswordMailUseCase {
   constructor(
-    @inject("UsersRepository") private usersRepository: IUsersRepository,
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
     @inject("UsersTokensRepository")
     private usersTokenRepository: IUsersTokensRepository,
-    @inject("DayjsDateProvider") private dateProvider: IDateProvider
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider,
+    @inject("EtherealMailProvider")
+    private mailProvider: IMailProvider
   ) {}
 
-  async execute(email: string) {
+  async execute(email: string): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) throw new HttpError("User does not exists!");
@@ -29,6 +34,12 @@ class SendForgotPasswordMailUseCase {
       refresh_token: token,
       expires_date,
     });
+
+    await this.mailProvider.sendMail(
+      email,
+      "Recuperação de Senha",
+      `O link para o reset é ${token}`
+    );
   }
 }
 
