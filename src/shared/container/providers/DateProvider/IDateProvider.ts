964392a31fba0ab835367b34d25e3efcac01b8cd
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-22 12:52:06 -0300

    feat: creates compare if is before method

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -4,6 +4,7 @@ interface IDateProvider {
   addHours(hours: number): Date;
   compareInHours(start_date: Date, end_date: Date): number;
   compareInDays(start_date: Date, end_date: Date): number;
+  compareIfBefore(start_date: Date, end_date: Date): boolean;
   convertToUTC(date: Date): string;
 }
 
--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -6,6 +6,18 @@ import { IDateProvider } from "../IDateProvider";
 dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
+  dateNow(): Date {
+    return dayjs().toDate();
+  }
+
+  addDays(days: number): Date {
+    return dayjs().add(days, "days").toDate();
+  }
+
+  addHours(hours: number): Date {
+    return dayjs().add(hours, "hour").toDate();
+  }
+
   compareInDays(start_date: Date, end_date: Date): number {
     const start_date_utc = this.convertToUTC(start_date);
     const end_date_utc = this.convertToUTC(end_date);
@@ -19,20 +31,12 @@ class DayjsDateProvider implements IDateProvider {
     return dayjs(end_date_utc).diff(start_date_utc, "hours");
   }
 
+  compareIfBefore(start_date: Date, end_date: Date): boolean {
+    return dayjs(start_date).isBefore(end_date);
+  }
+
   convertToUTC(date: Date): string {
     return dayjs(date).utc().local().format();
-  }
-
-  dateNow(): Date {
-    return dayjs().toDate();
-  }
-
-  addDays(days: number): Date {
-    return dayjs().add(days, "days").toDate();
-  }
-
-  addHours(hours: number): Date {
-    return dayjs().add(hours, "hour").toDate();
   }
 }
 
