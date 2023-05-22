a709943376311ffed4ea0cf862f59202726592bc
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-22 12:50:30 -0300

    feat: creates path mapping

--- src/modules/accounts/useCases/index.ts
+++ src/modules/accounts/useCases/index.ts
@@ -1,5 +1,7 @@
 export * from "./authenticateUser/AuthenticateUserUserCase";
 export * from "./createUser/CreateUserUseCase";
 export * from "./refreshToken/RefreshTokenUseCase";
+export * from "./resetPassword/ResetPasswordUseCase";
 export * from "./sendForgotPasswordMail/SendForgotPasswordMailUseCase";
 export * from "./updateUserAvatar/UpdateUserAvatarUseCase";
+
