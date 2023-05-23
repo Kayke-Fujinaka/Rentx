dbe82dd50f547ce18907f939eebea526ea05a0d6
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-23 11:15:44 -0300

    feat: validate by token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
 import auth from "@config/auth";
-import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { HttpError } from "@shared/errors/HttpError";
 
 interface IPayload {
@@ -14,25 +13,16 @@ export async function ensureAuthenticated(
   response: Response,
   next: NextFunction
 ): Promise<void> {
-  const { secret_refresh_token } = auth;
+  const { secret_token } = auth;
 
   const authHeader: string | undefined = request.headers.authorization;
-
-  const usersTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) throw new HttpError("Token missing!", 401);
 
   const [, token]: string[] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(token, secret_refresh_token) as IPayload;
-
-    const user = await usersTokensRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token
-    );
-
-    if (!user) throw new HttpError("User doesn't exists!", 401);
+    const { sub: user_id } = verify(token, secret_token) as IPayload;
 
     request.user = {
       id: user_id,
